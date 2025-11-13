3a34e731abc02ab433c5ae71407ffc20017dd32f
Kylen Xiao <ZeldaSpider599@gmail.com>
2025-11-12 22:31:27 -0700

    Fixed inventory not being reset on new game.

--- src/scripts/menu/main_menu_buttons.gd
+++ src/scripts/menu/main_menu_buttons.gd
@@ -10,11 +10,13 @@ extends VBoxContainer
 ## Starts a new game and loads into the game scene
 func _new_game() -> void:
 	get_tree().change_scene_to_file(game_scene)
+	InventoryManager.clear_data()
 	SceneManager.load_start_room()
 	
 ## Loads data from save file and enters game scene
 func _load_game() -> void:
 	get_tree().change_scene_to_file(game_scene)
+	SceneManager.load_start_room()
 	
 ## Loads credits scene
 func _goto_credits() -> void:
